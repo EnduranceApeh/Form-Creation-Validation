1137e8414edb0634249ae0c9b74cce7668efd8ca
EnduranceApeh <apehendurance2@gmail.com>
2025-11-28 12:40:51 +0100

    Modified script and index file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DomeContentLoaded', function () {
+document.addEventListener('DomContentLoaded', function () {
   const form = document.getElementById('registration-form');
   const feedbackDiv = document.getElementById('form-feedback');
 
